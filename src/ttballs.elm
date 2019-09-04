0926037569359ff7d9718452c51ac55c83be260a
Steven Henry Klaiber Noble <steven.noble@gmail.com>
2019-09-03 17:29:20 -0700

    pause button kinda works

--- src/ttballs.elm
+++ src/ttballs.elm
@@ -7,8 +7,10 @@ import Browser
 import Browser.Events
 import Html exposing (..)
 import Html.Attributes as H
+import Html.Events exposing (onClick)
 import Task
 import Time
+import Platform.Sub as Sub
 import Debug
 
 
@@ -34,6 +36,7 @@ type alias Model =
     , v0 : Vec2
     , x0 : Vec2
     , acc : Vec2
+    , paused : Bool
     }
 
 
@@ -54,7 +57,7 @@ init _ =
         acc =
             accFromV v0
     in
-        ( Model Nothing 0 v0 x0 acc
+        ( Model Nothing 0 v0 x0 acc False
         , Task.perform SetTime Time.now
         )
 
@@ -66,6 +69,7 @@ init _ =
 type Msg
     = Tick Time.Posix
     | SetTime Time.Posix
+    | Pause Bool
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -87,6 +91,9 @@ update msg model =
             , Cmd.none
             )
 
+        Pause paused ->
+            ( { model | paused = not (Debug.log "Pause" paused) }, Cmd.none )
+
 
 
 -- SUBSCRIPTIONS
@@ -94,7 +101,7 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Browser.Events.onAnimationFrame Tick
+    if model.paused then Sub.none else Browser.Events.onAnimationFrame Tick
 
 
 
@@ -127,10 +134,33 @@ view model =
 
         relTimeString =
             Basics.min model.relativeTime (maxRange |> toFloat) |> String.fromFloat
+
+        endVOverStartV =
+            0
+
+        ksx1 =
+            0.333
+
+        ksx2 =
+            0.667
+
+        ksy1 =
+            2.0 / (3.0 * (1.0 + endVOverStartV))
+
+        ksy2 =
+            ksy1 + 0.333
     in
         div []
-            [ input [ H.type_ "range", H.min "0", H.max (duration |> ceiling |> String.fromInt), H.value relTimeString, H.readonly True ] []
-            , svg [ Svg.Attributes.style "position:fixed; top:0; left:0; height:100%; width:100%" ]
+            [ input [ H.type_ "range", H.min "0", H.max (duration |> ceiling |> String.fromInt), H.value relTimeString, H.readonly model.paused ] []
+            , button [ onClick (Pause model.paused), H.readonly False ]
+                [ Html.text
+                    (if model.paused then
+                        "▶"
+                     else
+                        "⏸"
+                    )
+                ]
+            , svg [ Svg.Attributes.style "position:fixed; top: 50px; left:0; height:100%; width:100%" ]
                 [ Svg.circle
                     [ cx xString
                     , cy yString
@@ -141,7 +171,7 @@ view model =
                         [ Svg.Attributes.path <| String.join " " [ "M", xString, yString, "L", endPosXString, endPosYString ]
                         , keyPoints "0;1"
                         , keyTimes "0;1"
-                        , keySplines "0.333 0.667 0.667 1"
+                        , keySplines ([ ksx1, ksy1, ksx2, ksy2 ] |> List.map String.fromFloat |> String.join " ")
                         , calcMode "spline"
                         , fill "freeze"
                         , dur <| String.concat [ duration |> round |> String.fromInt, "ms" ]
